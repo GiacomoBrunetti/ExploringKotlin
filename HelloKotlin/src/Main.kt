569aaeab79d90fe09dd3224b610e97979fcb27a1
Giacomo Brunetti <giacomobrunetti6875@gmail.com>
2020-06-28 11:56:20 +0100

    the beginning of everything

---
+++ HelloKotlin/src/Main.kt
@@ -0,0 +1,32 @@
+
+fun forStatement() {
+    val names = arrayOf("Giacomo", "Giulia", null)
+    names.forEach { name -> greet(name) }
+}
+
+fun whenStatement(name: String?): String {
+    return when (name) {
+        "Giacomo" -> "Hi"
+        "Giulia" -> "Hey"
+        else -> "Error"
+    }
+}
+
+fun ifStatement(name: String?): String {
+//    return if (name != null) {
+//        name
+//    } else {
+//        "Name was not specified."
+//    }
+    return name ?: "Name was not specified."
+}
+
+fun greet(name: String?) = println("${whenStatement(name)} ${ifStatement(name)}")
+
+fun main() {
+    val hello: String = "Hello World!"
+    println(hello)
+    forStatement()
+}
+
+
